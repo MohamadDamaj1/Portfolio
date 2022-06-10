a8255b6e9c6579957355acca54432dc3928a5349
MohamadDamaj <mdamaj12@outlook.com>
2022-06-10 15:27:10 +0100

    a

--- scripts/script.js
+++ scripts/script.js
@@ -2,19 +2,21 @@ const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper')
 
 portfolioItems.forEach(portfolioItem => {
     portfolioItem.addEventListener('mouseover', () => {
-        console.log(portfolioItem.childNodes[1].classList);
+        (portfolioItem.childNodes[1].classList);
         portfolioItem.childNodes[1].classList.add('img-darken');
 
     })
 }
     )
 
-    
+    portfolioItem.addEventListener('mouseout', () => {
+        (portfolioItem.childNodes[1].classList);
+        portfolioItem.childNodes[1].classList.add('img-darken');
 
-   
+    })
        
-       
-   
+      
+
 
 
    
