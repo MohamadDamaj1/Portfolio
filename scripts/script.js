4c832c42b08284725e0aae453def84f356c9e7a7
MohamadDamaj <mdamaj12@outlook.com>
2022-06-10 15:13:53 +0100

    a

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ const portfolioItems = document.querySelectorAll('.portfolio-item-wrapper')
 
 portfolioItems.forEach(portfolioItem => {
     portfolioItem.addEventListener('mouseover', () => {
-         console.log(portfolioItem)
+         console.log(portfolioItem);
     })
 }
     )
